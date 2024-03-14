0af7805099e2fba7eaf54cffb0d837a959dce78d
Cuma Bolat <cuma.bolat@toptal.com>
2024-03-14 17:31:16 +0300

    added command line interface

--- src/main/java/com/jsearch/App.java
+++ src/main/java/com/jsearch/App.java
@@ -1,11 +1,45 @@
 package com.jsearch;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    printJSearchAsciiArt();
+    Scanner scanner = new Scanner(System.in);
+
+    while (true) {
+      System.out.println("Please enter the query you want to search \r");
+      String query = scanner.next();
+      if (query.equals("q")) break;
+
+      System.out.println("Please enter the search directory name \r");
+      System.out.println("(Press a for the whole computer) \r");
+      String directoryPath = scanner.next();
+
+      if (directoryPath.equals("a")) {
+        // Do something bla bla..
+        System.out.println(query);
+      } else {
+        System.out.println(directoryPath);
+        // Do something bla bla..
+      }
+    }
+  }
+
+  private static void printJSearchAsciiArt() {
+    System.out.println("# Welcome To \n" + 
+                      "#                                                          \n" +
+                      "#       ██╗███████╗███████╗ █████╗ ██████╗  ██████╗██╗  ██╗\n" +
+                      "#       ██║██╔════╝██╔════╝██╔══██╗██╔══██╗██╔════╝██║  ██║\n" +
+                      "#       ██║███████╗█████╗  ███████║██████╔╝██║     ███████║\n" +
+                      "#  ██   ██║╚════██║██╔══╝  ██╔══██║██╔══██╗██║     ██╔══██║\n" +
+                      "#  ╚█████╔╝███████║███████╗██║  ██║██║  ██║╚██████╗██║  ██║\n" +
+                      "#   ╚════╝ ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝\n" +
+                      "#                                                          \n" +
+                      "#                                         made by cumabolat\n");
   }
 }
