f92ff2b3c2893c64a3be92460230ee9ec050fa3a
Cuma Bolat <cuma.bolat@toptal.com>
2024-03-20 19:55:00 +0300

    implemented csv indexing strategy

--- src/main/java/com/jsearch/indexer/indexing/strategies/CsvIndexingStrategy.java
+++ src/main/java/com/jsearch/indexer/indexing/strategies/CsvIndexingStrategy.java
@@ -1,11 +1,39 @@
 package com.jsearch.indexer.indexing.strategies;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 import com.jsearch.indexer.indexing.FileIndexer;
 
 public class CsvIndexingStrategy extends FileIndexer {
+
+  /*
+   * Need optimization. Currently, applying regex takes a lot of time.
+   */
   @Override
   public void index(File file) {
+    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+      String line;
+      String filePath = file.getPath();
+      int lineNumber = 0;
+
+      while ((line = reader.readLine()) != null) {
+        lineNumber++;
+        String[] cells = line.split(",");
+        for (int i = 0; i < cells.length; i++) {
+          String[] words = cells[i].split(" ");
+          for (int j = 0; j < words.length; j++) {
+            String word = words[j];
+            System.out.println("Adding word: " + word);
+
+            addWordToIndex(word, filePath, lineNumber, i);
+          }
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
